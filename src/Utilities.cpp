9c6aa1bd233b664cc749d851255ffce390c9888b
Aleksandr Vasilenko <vasilenko.alex93@gmail.com>
2015-11-29 15:59:55 -0800

    Moved implementations of Utilitity functions to cpp file

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,1 +1,43 @@
 #include "Utilities.h"
+
+void _CommandInput::getInput()
+{
+    string buffer;
+    bool valid_input = false;
+    while( !valid_input )
+    {
+        printf("(Enter a command)---> ");
+        //cin.ignore(INT_MAX);
+        //cin.clear();
+        getline(cin, buffer);
+        for( int i = 0; i < 25; i++ )
+        {
+            if( buffer[i] == '\n' || buffer[i] == '\0' )
+                break;
+            if( isalpha(buffer[i]) )
+            {
+                valid_input = true;
+                command = buffer[i];
+                break;
+            }
+        }
+        if( !valid_input )
+            printf("Your input was invalid, please try again.\n" );
+    }
+}
+
+void _CommandArgumentInput::getInput()
+{
+    char buffer[25];
+    cin.get( buffer, 24 );
+}
+
+void _CommandNumberInput::getInput()
+{
+    //TODO
+}
+
+void _NumberNumberInput::getInput()
+{
+    //TODO
+}
--- src/Utilities.h
+++ src/Utilities.h
@@ -7,70 +7,34 @@
 
 using namespace std;
 
-typedef struct
-{
+struct _CommandInput{
     char command;
 
-    void getInput()
-    {
-        string buffer;
-        bool valid_input = false;
-        while( !valid_input )
-        {
-            printf("(Enter a command)---> ");
-            //cin.ignore(INT_MAX);
-            //cin.clear();
-            getline(cin, buffer);
-            for( int i = 0; i < 25; i++ )
-            {
-                if( buffer[i] == '\n' || buffer[i] == '\0' )
-                    break;
-                if( isalpha(buffer[i]) )
-                {
-                    valid_input = true;
-                    command = buffer[i];
-                    break;
-                }
-            }
-            if( !valid_input )
-                printf("Your input was invalid, please try again.\n" );
-        }
-    }
-
-} CommandInput;
-
-typedef struct
-{
+    void getInput();
+};
+struct _CommandArgumentInput{
     char command;
     char argument;
 
-    void getInput()
-    {
-        char buffer[25];
-        cin.get( buffer, 24 );
-    }
-} CommandArgumentInput;
-
-typedef struct
-{
+    void getInput();
+};
+struct _CommandNumberInput{
     char command;
     int  number;
 
-    void getInput()
-    {
-        //TODO
-    }
-} CommandNumberInput;
-
-typedef struct
-{
+    void getInput();
+};
+struct _NumberNumberInput{
     int number1;
     int number2;
 
-    void getInput()
-    {
-        //TODO
-    }
-} NumberNumberInput;
+    void getInput();
+};
+
+
+typedef struct _CommandInput CommandInput;
+typedef struct _CommandArgumentInput CommandArgumentInput;
+typedef struct _CommandNumberInput CommandNumberInput;
+typedef struct _NumberNumberInput NumberNumberInput;
 
 #endif
